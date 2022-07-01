2c3c7b789f6b9a9d88ca61f2270ae401dfaee4fe
Ddlgc <86103938+DdLgc@users.noreply.github.com>
2022-07-01 19:50:35 +0200

    Exos sur les nombres JS

---
+++ JavaScript/lesnombres.js
@@ -0,0 +1,46 @@
+const note = 4.1
+console.log(note)
+
+/** JavaScript */
+console.log(typeof(123456))
+console.log(typeof(-4))
+console.log(typeof(3.14159))
+
+console.log('-------op de base----------')
+
+const nbMeals =  3
+const nbDays = 7
+console.log(nbMeals * nbDays)
+
+const famille1 = 1
+const famille2 = 3
+const famille3 = 4
+const chocolat = 7
+console.log((famille1 + famille2 + famille3) * chocolat)
+
+console.log('-------op avancées----------')
+
+const length1 = 3
+const length2 = 4
+const hyp = Math.sqrt(Math.pow(length1, 2) + Math.pow(length2, 2))
+console.log("Longueur de l'hypothenuse: " + hyp)
+
+/** Volume d'un cube**/
+console.log(Math.pow(5, 3))
+
+console.log('-------Précisions----------')
+
+const average = 14.8
+const note1 = 9.1
+console.log('Ecart: ', note1 - average)
+
+console.log(0.1 * 0.1)
+
+console.log('-------Booléens----------')
+
+console.log(true && false)
+console.log(!false) //Affiche l'inverse
+const str = ""
+const nb = 0
+console.log(str || nb)
+
