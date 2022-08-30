b1133f0c23de2d41bd7045a7d58644ec110c8f82
Ddlgc <86103938+DdLgc@users.noreply.github.com>
2022-08-30 10:01:42 +0200

    Défi JS sur les algorithmes

---
+++ Defi Studi/Defi JS/defi_algo-languages.js
@@ -0,0 +1,7 @@
+
+// Soit T le mot dont la longueur est à déterminer
+// Calculer N le nombre de lettres final, égal à 0
+// Pour toutes les lettres L de T
+//     Calculer N, qui est N + 1
+// Fin pour
+// Retourner N
